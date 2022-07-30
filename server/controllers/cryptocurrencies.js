590307bae0615f7200767a92c49b04d92e33353a
Dwight Smith <dwightsmth@gmail.com>
2022-07-30 18:05:56 -0400

    updated crypto server routes/controllers

--- server/controllers/cryptocurrencies.js
+++ server/controllers/cryptocurrencies.js
@@ -16,7 +16,7 @@ export const getCryptos = async (req, res) => {
     const { limit } = req.params;
 
     try {
-        const response = await fetch(`${baseUrl}/cryptocurrency/listings/latest?limit=${limit}`, reqOptions);
+        const response = await fetch(`${baseUrl}/v1/cryptocurrency/listings/latest?limit=${limit}`, reqOptions);
         const data = await response.json();
         res.json(data);
 
@@ -26,11 +26,25 @@ export const getCryptos = async (req, res) => {
 
 }
 
-export const getCrypto = async (req, res) => {
+export const getCryptoInfo = async (req, res) => {
     const { id } = req.params;
 
     try {
-        const response = await fetch(`${baseUrl}/cryptocurrency/info?id=${id}`, reqOptions);
+        const response = await fetch(`${baseUrl}/v1/cryptocurrency/info?id=${id}`, reqOptions);
+        const data = await response.json();
+        res.json(data);
+
+    } catch(err) {
+        return console.error('server request failed: ', err);
+    }
+}
+
+
+export const getCryptoQuote = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const response = await fetch(`${baseUrl}/v2/cryptocurrency/quotes/latest?id=${id}`, reqOptions);
         const data = await response.json();
         res.json(data);
 
--- server/routes/cryptocurrencies.js
+++ server/routes/cryptocurrencies.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getCryptos, getCrypto } from '../controllers/cryptocurrencies.js';
+import { getCryptos, getCryptoInfo, getCryptoQuote } from '../controllers/cryptocurrencies.js';
 
 const router = express.Router();
 
 router.get('/:limit', getCryptos);
-router.get('/details/:id', getCrypto);
+router.get('/info/:id', getCryptoInfo);
+router.get('/quotes/:id', getCryptoQuote);
 
 export default router;
