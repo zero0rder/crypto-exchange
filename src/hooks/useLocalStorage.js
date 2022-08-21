d2cf19d97d4749974395283ff0ed2dc4f87a9d03
Dwight Smith <dwightsmth@gmail.com>
2022-08-21 19:06:27 -0400

    added hook for local storage support

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+
+const getSavedValue = (key, value) => {
+    const res = JSON.parse(localStorage.getItem(key));
+
+    if(res) return res;
+    if(value instanceof Function) return value();
+
+    return value;
+}
+
+const useLocalStorage = (key, val) => {
+    const [ storedValue, setStoredvalue ] = useState(() => getSavedValue(key, val));
+
+    useEffect(() =>{
+        localStorage.setItem(key, JSON.stringify(storedValue));
+    }, [storedValue, key])
+
+    return [storedValue, setStoredvalue];
+}
+
+
+export default useLocalStorage;
