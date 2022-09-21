bc0a43c7600326a616cba59bf9a579902402149a
Dwight Smith <dwightsmth@gmail.com>
2022-09-20 22:39:36 -0400

    404 page

--- src/components/layout/notFound.js
+++ src/components/layout/notFound.js
@@ -1,4 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button, Result } from 'antd';
 
-const NotFound = () => <h1>Page Not Found!</h1>;
-export default NotFound;
+const NotFoundContent = () => {
+    const navigate = useNavigate()
+    
+    return (
+        <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={<Button onClick={() => navigate('/')} type="primary">Back Home</Button>}
+      />
+    )
+}
+
+const NotFound = () => <NotFoundContent/>
+export default NotFound
