883067c6f1b6c8f82faf51c0d9544461caebd004
Dwight Smith <dwightsmth@gmail.com>
2022-12-02 16:10:29 -0500

    chore(Footer): footer html layout formatting

--- src/components/layout/mainFooter.js
+++ src/components/layout/mainFooter.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import { CopyrightOutlined } from '@ant-design/icons';
-import { Layout } from 'antd';
-const { Footer } = Layout;
+import React from 'react'
+import { CopyrightOutlined } from '@ant-design/icons'
+import { Layout } from 'antd'
+const { Footer } = Layout
 
 const MainFooter = () => {
-
     return (
         <Footer className="site-footer">
             <span>Crypto Exchange App</span>
             <span>All Rights Reserved <CopyrightOutlined /> {new Date().getFullYear()}</span>
         </Footer>
     )
-};
+}
 
-export default MainFooter;
+export default MainFooter
