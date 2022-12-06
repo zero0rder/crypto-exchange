8a7a8b3590feff3293c3fb8b5da1a572d32d6bdc
Dwight Smith <dwightsmth@gmail.com>
2022-12-06 11:59:09 -0500

    chore(UI): updated footer style + formatting

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,36 +1,36 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 const handleStorage = (key, value) => {
     if(typeof window === 'undefined')
-        return value;
+        return value
 
     try{
-        const res = localStorage.getItem(key);
-        return res ? JSON.parse(res) : value;
+        const res = localStorage.getItem(key)
+        return res ? JSON.parse(res) : value
 
     } catch(err){
-        console.error(err);
-        return value;
+        console.error(err)
+        return value
     }
 }
 
 const useLocalStorage = (key, val) => {
-    const [ storedValue, setStoredValue ] = useState(() => handleStorage(key, val));
+    const [ storedValue, setStoredValue ] = useState(() => handleStorage(key, val))
 
     const setValue = (value) => {
         try{
-            const valueToStore = value instanceof Function ? value(storedValue) : value;
-            setStoredValue(valueToStore);
+            const valueToStore = value instanceof Function ? value(storedValue) : value
+            setStoredValue(valueToStore)
 
             if(typeof window !== 'undefined')
-                localStorage.setItem(key, JSON.stringify(valueToStore));
+                localStorage.setItem(key, JSON.stringify(valueToStore))
 
         } catch(err){
             console.error(err)
         }
     }
 
-    return [storedValue, setValue];
+    return [storedValue, setValue]
 }
 
-export default useLocalStorage;
+export default useLocalStorage
