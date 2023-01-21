76b9dbddec58d69c89a0eb1be9e0e4f5599ec4f8
Dwight Smith <dwightsmth@gmail.com>
2023-01-20 19:45:52 -0500

    fix: added redis prod config

--- server/index.js
+++ server/index.js
@@ -9,9 +9,14 @@ import GlobalRoutes from './routes/crypto/globalMetrics.js';
 import UserRoutes from './routes/users/user.js';
 
 //redis cache
-const cacheClient = createClient();
-cacheClient.on('error', (e) => console.error(e));
-await cacheClient.connect();
+const redis = new createClient({
+    url: process.env.REDIS_URL,
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PW
+});
+
+redis.on('error', (e) => console.error(e));
+await redis.connect();
 
 const app = express();
 dotenv.config();
@@ -33,14 +38,13 @@ mongoose.connect(process.env.CONNECTION_URL)
 
 async function getOrSetCache(req, res, next){
     const { originalUrl } = req,
-    cachedRes = await cacheClient.get(originalUrl);
+    cachedRes = await redis.get(originalUrl);
 
     if(cachedRes) {
         res.json(JSON.parse(cachedRes));
-        
     } else {
         req.isCached = false;
-        req.setCacheItem = async (data) => await cacheClient.set(originalUrl, JSON.stringify(data), {
+        req.setCacheItem = async (data) => await redis.set(originalUrl, JSON.stringify(data), {
             EX: 3600, // cache duration (seconds)
             NX: true, // only set non-existent keys
         });
